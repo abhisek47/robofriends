6e4f99e80fe61b2dae77e0474ae10d73b8e1df41
abhisek47 <abhisek529@gmail.com>
2020-05-23 02:17:47 +0530

    Updates

---
+++ precache-manifest.2b8761353df53894345cb8e62d2a55bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd618a202e4de11f855ee163ee1cd328",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b7a308b77c01497e6639",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e52e9e81f250cb112c7f",
+    "url": "/robofriends/static/css/main.1f4b84da.chunk.css"
+  },
+  {
+    "revision": "b7a308b77c01497e6639",
+    "url": "/robofriends/static/js/2.bf515174.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.bf515174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52e9e81f250cb112c7f",
+    "url": "/robofriends/static/js/main.9222ac0c.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
